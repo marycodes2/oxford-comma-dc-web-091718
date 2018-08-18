26366ba20dcb44738641f9a2fec691f2ecddde46
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-08-18 23:03:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,27 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    lengths = []
+    for x in array do
+      x = x.to_s
+      lengths << x.length
+    end
+    length_of_last_elem_plus_2 = -(lengths[-1] + 2)
+    puts "need to erase this int " + length_of_last_elem_plus_2.to_s
+    array = array.insert(-2, "and")
+    array = array.join(", ")
+    erased_comma = array.slice!(length_of_last_elem_plus_2)
+    puts array
+    return array
+  elsif array.length == 2
+    array = array.insert(-2, "and")
+    array = array.join(" ")
+    puts array
+    array
+  else
+    array = array.join
 end
+end
+
+
+array = ['mary', 'kevin']
+oxford_comma(array)
