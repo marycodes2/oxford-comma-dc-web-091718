ad7333b6b28a4a027f13c78f9e3a7107906abb0c
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-08-18 22:41:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,19 @@
 def oxford_comma(array)
   if array.length > 2 do
+    lengths = []
+    for x in array do
+      x = x.to_s
+      lengths << x.length
+    end
+    length_of_last_elem_plus_2 = length[-1] + 2
+    puts "need to erase this int" + length_of_last_elem_plus_2
     array = array.insert(-2, "and")
     array = array.join(", ")
     puts array
     return array
 end
+end
+
 
 array = ['mary', 'joseph', 'mike', 'kevin']
 oxford_comma(array)
